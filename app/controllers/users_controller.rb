59a1c5784a40a05331e2a1e76951b36768026185
muhammadullah <muhammadhullah23@gmail.com>
2022-04-27 02:13:02 -0400

    create method for User

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,13 @@
 class UsersController < ApplicationController
     # configure users to login and signup using JWT authenticity
+
+    def create
+        @user = User.create(user_params)
+        if @user.valid?
+            render json: { user: UserSerializer.new(@user)}, status: :created
+        else
+            render json: { error: 'failed to create user' }, status: :unprocessable_entity
+        end
+    end
+    
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,8 @@ class User < ApplicationRecord
     valitades_uniqueness_of :email, :username
     validates_format_of :email, with: URI::MailTo::EMAIL_REGEXP
     # URI::MailTo::EMAIL_REGEXP is a class provided by the standard Ruby library which declares the regular expression EMAIL_REGEXP.
-    
+
+    # TODO: change solution to validating an email to check if it actually exists.
 
 
 end
