abbaf2f4ff5cf37fb03a771946adb59c90c57b44
muhammadullah <muhammadhullah23@gmail.com>
2022-04-29 01:28:05 -0400

    product_params method

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,8 @@
 class ProductsController < ApplicationController
+
+    private
+
+    def product_params
+        params.require(:product).permit(:user_id, :name, :brand, :price_bought, :shipping_price, :sales_tax, :price_sold, :shipping_fee, :additional_fees, :expenses, :profit)
+    end
 end
