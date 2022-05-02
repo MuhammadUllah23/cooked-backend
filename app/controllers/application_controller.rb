34a4805d54747245318a62b6a5382d9d1df9ff3d
muhammadullah <muhammadhullah23@gmail.com>
2022-05-02 18:11:39 -0400

    set up server to anticipate a JWT sent along in request headers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,9 +11,15 @@ class ApplicationController < ActionController::API
     end
 
     def decoded_token(token)
-        # token => "eyJhbGciOiJIUzI1NiJ9.eyJiZWVmIjoic3RlYWsifQ._IBTHTLGX35ZJWTCcY30tLmwU9arwdpNVxtVU0NpAuI"
-        JWT.decode(token, 'my_s3cr3t')[0]
-        # JWT.decode => [{ "beef"=>"steak" }, { "alg"=>"HS256" }]
-        # [0] gives us the payload { "beef"=>"steak" }
+        if auth_header
+            token = auth_header.split(' ')[1]
+            # headers: { 'Authorization': 'Bearer <token>' }
+            begin
+                JWT.decode(token, 'my_s3cr3t', tru, algorithm: 'HS256')
+                # JWT.decode => [{ "beef"=>"steak" }, { "alg"=>"HS256" }]
+            rescue JWT::DecodeError
+                nil
+            end
+        end
     end
 end
