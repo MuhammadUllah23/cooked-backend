3fe711bf2051635575e37ecc492b3a0472f10139
muhammadullah <muhammadhullah23@gmail.com>
2022-05-02 19:18:03 -0400

    Users create method issues token when user is created

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,12 @@
 class UsersController < ApplicationController
-    # configure users to login and signup using JWT authenticity
+    
 
+    # Creats a new user  and issues a token if it has been sucessfully created.
     def create
         @user = User.create(user_params)
         if @user.valid?
-            render json: { user: UserSerializer.new(@user)}, status: :created
+            @token = encoded_token(user_id: @user.id)
+            render json: { user: UserSerializer.new(@user), jwt: @token}, status: :created
         else
             render json: { error: 'failed to create user' }, status: :unprocessable_entity
         end
