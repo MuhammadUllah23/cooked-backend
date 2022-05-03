201d5edfa7982575c4767be73e566cca2ede4a94
muhammadullah <muhammadhullah23@gmail.com>
2022-05-03 01:52:49 -0400

    define auth create method for users to log in

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,6 +1,18 @@
 class AuthController < ApplicationController
     skip_before_action :authorized, only [:create]
 
+    def create 
+        @user = User.find_by(username: user_login_params[:username])
+
+        if @user && @user.authenticate(user_login_params[:password])
+            # encode token comes from Application Controller
+            token = encoded_token({user: @user.id})
+            render json: { user: UserSerializer.new(@user), jwt: token }, status: :accepted
+        else
+             render json: { message: 'Invalid username or password' }, status: :unauthorized
+        end
+    end
+
     private
 
     def user_login_params
