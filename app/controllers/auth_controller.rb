0d3b4a50b51ee25ce9cfc389191e9fe9b9da34f7
muhammadullah <muhammadhullah23@gmail.com>
2022-05-03 01:47:03 -0400

    define user_login_params method

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,4 +1,10 @@
 class AuthController < ApplicationController
     skip_before_action :authorized, only [:create]
 
+    private
+
+    def user_login_params
+        # params { user: {username: 'Chandler Bing', password: 'hi' } }
+        params.require(:user).permit(:username, :password)
+    end
 end
